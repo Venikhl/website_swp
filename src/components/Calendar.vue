4168a534d35eb87956de04849a078dbdaa58abc8
venikhl <g.pavlov2019@gmail.com>
2023-07-16 16:48:26 +0300

    editEventPopup not working yet.

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="calendarInfo">
     <FullCalendar :options="calendarOptions" />
+    <!-- Add the popup when an event is clicked -->
+    <EditEventPopup v-if="clickedEvent" :event="clickedEvent" @close-popup="closePopup" />
   </div>
 </template>
 
@@ -8,10 +10,12 @@
 import FullCalendar from '@fullcalendar/vue3';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import axios from 'axios';
+import EditEventPopup from './EditEventPopup.vue';
 
 export default {
   components: {
-    FullCalendar
+    FullCalendar,
+    EditEventPopup,
   },
   props: ['group'],
   data() {
@@ -19,9 +23,12 @@ export default {
       calendarOptions: {
         plugins: [dayGridPlugin],
         initialView: 'dayGridMonth',
-        weekends: true, // Изменено на true
-        events: []
-      }
+        weekends: true,
+        events: [],
+        // Add eventClick handler here
+        eventClick: this.showEditEventPopup,
+      },
+      clickedEvent: null,
     };
   },
   methods: {
@@ -32,23 +39,24 @@ export default {
       try {
         const response = await axios.get(apiUrl);
         const eventData = response.data;
-        print(console.log(eventData))
 
-        const events = eventData.map(event => ({
+        const events = eventData.map((event) => ({
+          id: event.id,
           title: event.title,
           start: new Date(event.date),
-          // extendedProps: {
-          //   recurring: {
-          //     daysOfWeek: [0, 1, 2, 3, 4, 5, 6]
-          //   }
-          // }
         }));
-        // console.log(events)
 
         this.calendarOptions.events = events;
       } catch (error) {
         console.error('Ошибка при получении данных с API:', error);
       }
+    },
+    showEditEventPopup(info) {
+      const clickedEventId = info.event.id;
+      this.clickedEvent = this.calendarOptions.events.find((event) => event.id === clickedEventId);
+    },
+    closePopup() {
+      this.clickedEvent = null;
     },
   },
   watch: {
@@ -56,9 +64,9 @@ export default {
       immediate: true,
       handler(newGroup) {
         this.fetchEvents();
-      }
-    }
-  }
+      },
+    },
+  },
 };
 </script>
 
@@ -74,11 +82,9 @@ export default {
   background-color: #F9B634 !important;
   border-color: #F9B634 !important;
   color: white;
-  width: 80%;
 }
 
 .fc-event-title {
   color: white;
-  font-size: 16px;
 }
 </style>
--- src/components/EditEventPopup.vue
+++ src/components/EditEventPopup.vue
@@ -1,54 +1,41 @@
 <template>
-    <div class="popup-container">
-      <h2>Edit Event</h2>
-      <label for="eventName">Event Name:</label>
-      <input v-model="eventName" type="text" id="eventName" />
-      <label for="eventDate">Event Date:</label>
-      <input v-model="eventDate" type="text" id="eventDate" />
-      <button @click="saveEventChanges">Save Changes</button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      event: {
-        type: Object,
-        required: true,
-      },
+  <div class="eventPopup">
+    <h2>Edit Event</h2>
+    <p><strong>Event ID:</strong> {{ event.id }}</p>
+    <p><strong>Event Title:</strong> {{ event.title }}</p>
+    <!-- Add more event details here as needed -->
+
+    <button @click="updateEvent">Update Event</button>
+    <button @click="closePopup">Close</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['event'],
+  methods: {
+    updateEvent() {
+      // Implement your logic to update the event here
+      // You can emit an event to notify the parent component (Calendar.vue) that the event is updated
+      // For example: this.$emit('event-updated', updatedEvent);
     },
-    data() {
-      return {
-        eventName: this.event.name,
-        eventDate: this.event.date,
-      };
+    closePopup() {
+      // Emit an event to notify the parent component (Calendar.vue) to close the popup
+      this.$emit('close-popup');
     },
-    methods: {
-      saveEventChanges() {
-        // Emit the updated event data with the changes
-        this.$emit('event-updated', {
-          ...this.event,
-          name: this.eventName,
-          date: this.eventDate,
-        });
-  
-        // Close the popup
-        this.$emit('close-popup');
-      },
-    },
-  };
-  </script>
-  
-  <style>
-  .popup-container {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: white;
-    padding: 20px;
-    border: 1px solid #ccc;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style>
+.eventPopup {
+  background-color: white;
+  border-radius: 10px;
+  padding: 10px;
+  color: black;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
